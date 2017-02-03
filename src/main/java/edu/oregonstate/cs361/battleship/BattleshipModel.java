6ec61d7fa270ead2d73d06eb6675c75b6199f5cc
jlioy <joshua.lioy@icloud.com>
2017-02-02 21:56:36 -0800

    Made changes to the BattleshipModel and added Ship and Point Classes

--- src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
+++ src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
@@ -4,32 +4,44 @@ package edu.oregonstate.cs361.battleship;
  * Created by michaelhilton on 1/26/17.
  */
 public class BattleshipModel {
-    private int[] ship;
-    private String ship_type;
+    //User ships
+    private Ship aircraftCarrier;
+    private Ship battleship;
+    private Ship cruiser;
+    private Ship destroyer;
+    private Ship submarine;
+    //Computer ships
+    private Ship computer_aircraftCarrier;
+    private Ship computer_battleship;
+    private Ship computer_destroyer;
+    private Ship computer_cruiser;
+    private Ship computer_submarine;
 
-    public BattleshipModel (int size) {
-        ship = new int[size];
-    }
+    private Point[] playerHits;
+    private Point[] playerMisses;
+    private Point[] computerHits;
+    private Point[] computerMisses;
 
-    public void setHit() {
-        for (int i = 0; i < ship.length; i++) {
-            if (ship[i] == 0) {
-                ship[i] = 1;
-                break;
-            }
-        }
-    }
+    public BattleshipModel () {
+        //Default start and end constructs
+        Point us = new Point(0,0);
+        Point ue = new Point(0,0);
+        //Creating user ships
+        aircraftCarrier = new Ship("AircraftCarrier", 5, us, ue);
+        battleship = new Ship("Battleship", 4, us, ue);
+        cruiser = new Ship("Cruiser", 3, us, ue);
+        destroyer = new Ship("Destroyer", 2, us, ue);
+        submarine = new Ship("Submarine", 2, us , ue);
+        //Creating computer ships and initializing to the values from the readme.md
+        computer_aircraftCarrier = new Ship("Computer_AircraftCarrier", 5, new Point(2,2), new Point(2,7));
+        computer_battleship = new Ship("Computer_Battleship", 4, new Point(2,8), new Point(6,8));
+        computer_cruiser = new Ship("Computer_Cruiser", 3, new Point(4,1), new Point(4,4));
+        computer_destroyer = new Ship("Computer_Destroyer", 2, new Point(7,3), new Point(7,5));
+        computer_submarine = new Ship("Computer_Submarine", 2, new Point(9,6), new Point(9,8));
 
-    public boolean isDestroyed() {
-        boolean destroyed = true;
-
-        for (int i = 0; i < ship.length && destroyed; i++) {
-            destroyed = ship[i] != 0;
-        }
-        return destroyed;
-    }
-
-    public int getSize() {
-        return ship.length;
+        playerHits = new Point[100];
+        playerMisses = new Point[100];
+        computerHits = new Point[100];
+        computerMisses = new Point[100];
     }
 }
