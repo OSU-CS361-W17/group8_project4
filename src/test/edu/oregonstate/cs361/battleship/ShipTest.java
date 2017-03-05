37dd89c886415b86251aeaf2cc8724eacccb0a87
chase cameron <chase cameron>
2017-03-04 20:16:02 -0800

    Added some test cases for civShips.

--- src/test/edu/oregonstate/cs361/battleship/ShipTest.java
+++ src/test/edu/oregonstate/cs361/battleship/ShipTest.java
@@ -10,11 +10,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShipTest {
     @Test
     public void testShip() {
-        Point p = new Point(0,0);
+        Point p = new Point(0, 0);
         Ship s = new Ship("cheese", 5, p, p);
         assertEquals("cheese", s.name);
         assertEquals(5, s.length);
-        assertEquals(p,s.start);
+        assertEquals(p, s.start);
         assertEquals(p, s.end);
     }
+}
+
+/**
+ * Created by Chase McWhirt 3/4/2017.
+ */
+
+class CivilianShipTest {
+    @Test
+    public void testCivShip() {
+        Point p = new Point(0, 0);
+        Ship s = new Ship("civShip", 3, p, p);
+        assertEquals("civShip", s.name);
+        assertEquals(3, s.length);
+        assertEquals(p, s.start);
+        assertEquals(p, s.end);
+        /*
+        Fire f = new Fire(0, 0);
+        assertEquals(1, s.hit);
+        assertEquals(0, s.health);
+         */
+    }
+
 }
